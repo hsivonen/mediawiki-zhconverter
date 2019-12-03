ca0e53740fc3eddca0a213a199b713f3060824f3
Henri Sivonen <hsivonen@hsivonen.fi>
2019-12-03 08:57:55 +0200

    Create a command-line program that converts from stdin to stdout.

---
+++ lineconvert.php
@@ -0,0 +1,18 @@
+﻿<?php
+
+/**
+ * Define the folder(note the ending slash) containing the mediawiki's files.
+ *  
+ * YOU SHOULD PLACE THEM OUTSIDE THE DOCUMENT ROOT OF YOUR WEB SERVER!!!
+ */
+
+define("MEDIAWIKI_PATH", "/opt/Projects/mediawiki-1.26.2/");
+
+/* Include our helper class */
+require_once "mediawiki-zhconverter.inc.php";
+
+while (false !== ($line = fgets(STDIN))) {
+    echo MediaWikiZhConverter::convert($line, $argv[1]);
+}
+
+?>
